7bcd82e720546e376933ae99ac810f74023c0cf8
OlyaMosienko <olya.ololoeva81@mail.ru>
2023-10-10 19:24:02 +0300

    Add script and style for tabs

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', ()=> {
     // Tabs Menu
     const tab = document.querySelectorAll('.tab-item');
     const tabParent = document.querySelector('.tabs-menu');
-    const tabContent = document.querySelectorAll('.tab-content');
+    const tabContent = document.querySelectorAll('.tabcontent');
 
     function hideTabContent() {
         tabContent.forEach(item => {
@@ -31,26 +31,4 @@ window.addEventListener('DOMContentLoaded', ()=> {
             });
         }
     });
-
-    // Получаем все элементы вкладок и контента
-// var tabItems = document.querySelectorAll('.tab-item');
-// var tabContents = document.querySelectorAll('.tab-content');
-
-// // Добавляем обработчик события клика на каждую вкладку
-// tabItems.forEach(function(item) {
-//   item.addEventListener('click', function() {
-//     // Удаляем класс 'current' у всех вкладок и контента
-//     tabItems.forEach(function(tab) {
-//       tab.classList.remove('current');
-//     });
-//     tabContents.forEach(function(content) {
-//       content.classList.remove('current');
-//     });
-    
-//     // Добавляем класс 'current' к выбранной вкладке и соответствующему контенту
-//     this.classList.add('current');
-//     var tabId = this.getAttribute('href');
-//     document.querySelector(tabId).classList.add('current');
-//   });
-// });
 });
